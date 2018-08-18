384234617c078f230c510066c7be60ef5db8261f
snelakanti <vnelakanti@gmail.com>
2018-08-17 22:36:49 -0400

    setup the store

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer); 
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(
+    <Provider store = {store}>
+<App />, 
+</Provider>,
+document.getElementById('root'));

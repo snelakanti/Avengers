538904f5be54e02f0d117bd3850c1410971276e0
snelakanti <vnelakanti@gmail.com>
2018-08-18 22:26:15 -0400

    setup first action

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { createStore} from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
+import { addCharacterById } from './actions';
 
 const store = createStore(rootReducer); 
 console.log('store.getState()', store.getState());
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
 
-ReactDOM.render(
+ ReactDOM.render( 
     <Provider store = {store}>
-<App />, 
+<App />
 </Provider>,
-document.getElementById('root'));
+document.getElementById('root')
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import characters_json from '../data/characters.json';
+import { ADD_CHARACTER } from '../actions';
 
 function characters(state = characters_json, action)  {
 
     switch(action.type){
+        case ADD_CHARACTER:
+            let characters  = state.filter(item => item.id !== action.id); 
+            return characters;
         default: 
         return state;
     }
